85319eadb467cf2d0c1da334c8f373492d2f50ba
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-11 01:39:34 +0600

    book route

--- be-library/src/routes/books.js
+++ be-library/src/routes/books.js
@@ -7,4 +7,22 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    res.status(200).json({
+        message: 'you did it'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const book = {
+        title: req.body.title,
+        category: req.body.category,
+        summary: req.body.summary,
+        author: req.body.author,
+    }
+    res.status(201).json({
+        message: book
+    });
+});
+
 module.exports = router;
