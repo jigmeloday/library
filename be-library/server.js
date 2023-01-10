a84294f178c0010302c355de8325ad5b5663d578
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-10 23:19:11 +0600

    route set up

--- be-library/server.js
+++ be-library/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const app = express();
+const indexRouter = require('./src/routes/index')
 
+app.use('/', indexRouter)
 app.listen(process.env.DEV_SERVER || 3000)
