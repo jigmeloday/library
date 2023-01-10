d37aa1052fc0f6f17ff69506378b319a0e27b95c
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-11 01:21:45 +0600

    book route

--- be-library/app.js
+++ be-library/app.js
@@ -3,16 +3,25 @@ const log = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
-// const book = require('./src/routes/authors');
+const book = require('./src/routes/books');
 const author = require('./src/routes/authors');
 
+
+//Logger
 app.use(log('dev'));
+
+//bodyParser
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+
+//route
+app.use('/authors', author);
 app.use('/authors', author);
 
+//error handler
 app.use((req, res, next) =>{
     const error = new Error('Not Found');
     error.status=404;
