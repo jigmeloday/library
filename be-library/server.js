4f4bccb65c978342043ba5217932cb68b198201d
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-11 00:05:15 +0600

    connection with mdb

--- be-library/server.js
+++ be-library/server.js
@@ -1,6 +1,18 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express();
 const indexRouter = require('./src/routes/index')
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DATABASE_URL)
+
+const db = mongoose.connection;
+
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('success'))
 
 app.use('/', indexRouter)
 app.listen(process.env.DEV_SERVER || 3000)
