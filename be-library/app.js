ae7ae31c418ba692d43747181ce7d54b0b8253dd
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-11 12:13:41 +0600

    bug fixes

--- be-library/app.js
+++ be-library/app.js
@@ -19,20 +19,16 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 //CORS
-
-app.use((req,res, next) =>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'DELETE', 'PATCH', 'GET');
-        return res.status(200).json({})
-    }
-
-})
-
+// app.use((req,res, next) =>{
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, PATCH, GET');
+//         return res.status(200).json({})
+//     }
+// });
 
 //route
-
 app.use('/authors', author);
 app.use('/books', book);
 
