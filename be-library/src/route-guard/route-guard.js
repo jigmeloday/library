1ffa120bab856a681f0e6a1c3763f6c0d0472d2f
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-11 23:29:44 +0600

    typo

--- be-library/src/route-guard/route-guard.js
+++ be-library/src/route-guard/route-guard.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
         next();
     } catch (error) {
         res.status(401).json({
-            message: 'Unautharized'
+            message: 'Unauthorized'
         });
     }
 };
