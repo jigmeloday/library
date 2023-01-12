b49dfe60d22a3a2327a6d20e3c27644e1ab102e2
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-12 21:29:24 +0600

    folder set up

--- fe-library/src/app/pages/home/home.tsx
+++ fe-library/src/app/pages/home/home.tsx
@@ -1,9 +1,7 @@
-import { memo } from 'react';
-
 export function Home() {
     return(
         <>hello Home</>
     )
 }
 
-export default memo(Home)
+export default Home;
---
+++ fe-library/src/app/route/core-route/core-route.tsx
@@ -0,0 +1,4 @@
+export function CoreRoute() {
+
+}
+export default CoreRoute;
