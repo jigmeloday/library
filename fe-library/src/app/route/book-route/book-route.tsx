22f17ec9cc980b2ed567af303d97da1f51a9d5ed
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-13 00:14:41 +0600

    book set up

--- fe-library/src/app/route/book-route/book-route.tsx
+++ fe-library/src/app/route/book-route/book-route.tsx
@@ -1,17 +1,20 @@
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { Routes, Route} from "react-router-dom";
 import { RouteModel } from '../../shared/models/shared.model';
+
+const Book = lazy(() => import('../../pages/book/book-listing'))
+const BookDetails = lazy(() => import('../../pages/book/book-detail'))
 
 export function BookRoute() {
     const CORE_ROUTE: RouteModel[] = [
         {
             id: '1',
-            component: <>Book</>,
+            component: <Book/>,
             route: '/'
         },
         {
             id: '2',
-            component: <>Book details</>,
+            component: <BookDetails/>,
             route: '/:id'
         }
     ];
