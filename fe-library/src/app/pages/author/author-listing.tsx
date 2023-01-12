19d8d92c95066c7dcbf78b59144c58c916642420
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-13 00:19:58 +0600

    author set up set up

---
+++ fe-library/src/app/pages/author/author-listing.tsx
@@ -0,0 +1,7 @@
+export function AuthorListing() {
+    return(
+        <>author</>
+    )
+}
+
+export default AuthorListing;
--- fe-library/src/app/route/author-route/author-route.tsx
+++ fe-library/src/app/route/author-route/author-route.tsx
@@ -1,17 +1,20 @@
-import { Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { Routes, Route} from "react-router-dom";
 import { RouteModel } from '../../shared/models/shared.model';
+
+const Authors = lazy(() => import('../../pages/author/author-listing'));
+const AuthorDetails = lazy(() => import('../../pages/author/author-details'));
 
 export function AuthorRoute() {
     const CORE_ROUTE: RouteModel[] = [
         {
             id: '1',
-            component: <>AuthorRoute</>,
+            component: <Authors/>,
             route: '/'
         },
         {
             id: '2',
-            component: <>AuthorRoute</>,
+            component: <AuthorDetails/>,
             route: '/:id'
         }
     ];
