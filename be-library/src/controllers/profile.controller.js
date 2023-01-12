55547015161d82e3844db0a7eaa68c15400fc50e
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-12 13:19:37 +0600

    user profile

--- be-library/src/controllers/profile.controller.js
+++ be-library/src/controllers/profile.controller.js
@@ -1,7 +1,7 @@
 const Profile = require('../models/profile')
 
 exports.get_profile = (req, res, next) => {
-    const id = req.param.id;
+    const id = req.params.id;
     Profile.findById(id).exec().then((profile) => {
         if (profile) {
             res.status(200).json({ profile })
@@ -9,4 +9,21 @@ exports.get_profile = (req, res, next) => {
             res.status(404).json({message: 'Not found'})
         }
     }).catch((error) => res.status(500).json({message: error}))
+}
+
+exports.update_profile = () => {
+    const id = req.params.id;
+    for (const ops of req.body) {
+        updateVal[ops.propName] = ops.value;
+    }
+    Profile.update({_id, id}, { $set: updateVal })
+        .exec()
+        .then((resp) =>{
+            if (resp) {
+                res.status(201).json({book: resp})
+            }else{
+                res.status(400).json({error: 'not working'})
+            }
+        })
+        .catch((error) => res.status(500).json({message:error }))
 }
