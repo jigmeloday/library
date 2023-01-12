3f471057cfaf09332ceb6b94723b6c41d7567a83
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-13 00:40:17 +0600

    state and store set up

--- fe-library/src/App.tsx
+++ fe-library/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import CoreRoute from './app/route/core-route/core-route';
+import { PaletteMode, ThemeProvider } from '@mui/material';
+import { theme } from './assest/theme';
+import { useSelector } from 'react-redux';
+import { selectCurrentTheme } from './app/services/states/shared-state/shared.slice';
 
 function App() {
+  const currentTheme = useSelector(selectCurrentTheme);
   return (
-    <CoreRoute />
+    <ThemeProvider theme={theme(currentTheme as PaletteMode)}>
+      <CoreRoute />
+    </ThemeProvider>
   );
 }
 
