9b706ea3cb86e2f701271c97c70a5c1669a7d172
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-13 01:33:47 +0600

    layout set up

--- fe-library/src/app/components/header/header.tsx
+++ fe-library/src/app/components/header/header.tsx
@@ -1,8 +1,24 @@
 import { memo } from 'react';
+import { HeaderContainer } from './header.style';
+import { Grid } from '@mui/material';
 
 export function HeaderComponent() {
     return(
-        <>header</>
+        <HeaderContainer>
+            <Grid item container direction='row'>
+                <Grid item container xs={5}>
+                    hello
+                </Grid>
+                <Grid item container xs={6} >
+                   <Grid item container xs={8} justifyContent='end'>
+                       hello
+                   </Grid>
+                    <Grid item container justifyContent='end' xs={4}>
+                        hello
+                    </Grid>
+                </Grid>
+            </Grid>
+        </HeaderContainer>
     )
 }
 
