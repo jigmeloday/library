589d129e01b4b91825630ce2a7c7c1178d21b451
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-12 23:19:47 +0600

    route set up

--- fe-library/src/app/route/core-route/core-route.tsx
+++ fe-library/src/app/route/core-route/core-route.tsx
@@ -4,29 +4,51 @@ import { RouteModel } from '../../shared/models/shared.model';
 import Home from '../../pages/home/home';
 
 export function CoreRoute() {
-    const isAuthenticated = false;
-    const AUTHENTICATED_ROUTE: RouteModel[] = [
+    const CORE_ROUTE: RouteModel[] = [
         {
             id: '1',
             component: <Home/>,
-            route: ''
+            route: '/'
+        },
+        {
+            id: '2',
+            component: <>Book</>,
+            route: '/books'
+        },
+        {
+            id: '3',
+            component: <>Book Details</>,
+            route: '/book/:id'
+        },
+        {
+            id: '4',
+            component: <>category</>,
+            route: '/categories'
+        },{
+            id: '5',
+            component: <>category Details</>,
+            route: '/category/:id'
+        },
+        {
+            id: '6',
+            component: <>Author</>,
+            route: '/Authors'
+        },{
+            id: '7',
+            component: <>Author</>,
+            route: '/Author/:id'
         }
     ];
-    const UNAUTHENTICATED_ROUTE: RouteModel[] = [
-        {
-            id: '1',
-            component: <Home/>,
-            route: ''
-        }
-    ]
+
     return(
         <Suspense fallback='loading...'>
             <Routes>
                 {
-                    (isAuthenticated ? AUTHENTICATED_ROUTE : UNAUTHENTICATED_ROUTE).map(({ id, route, component }) =>
+                    CORE_ROUTE.map(({ id, route, component }) =>
                         <Route key={`${route}+${id}`} path={route} element={component} />
                     )
                 }
+                <Route path={'*'} element={<>404</>} />
             </Routes>
         </Suspense>
     )
