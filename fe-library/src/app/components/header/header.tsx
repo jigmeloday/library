30e8f3832b0de388df33469cc76faf0fe784a426
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-13 01:39:03 +0600

    feat: set up

--- fe-library/src/app/components/header/header.tsx
+++ fe-library/src/app/components/header/header.tsx
@@ -1,17 +1,20 @@
 import { memo } from 'react';
 import { HeaderContainer } from './header.style';
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
+import { NAVIGATION } from './constant/header.constant';
 
 export function HeaderComponent() {
     return(
         <HeaderContainer>
             <Grid item container direction='row'>
                 <Grid item container xs={5}>
-                    hello
+                    Logo
                 </Grid>
                 <Grid item container xs={6} >
                    <Grid item container xs={8} justifyContent='end'>
-                       hello
+                       {
+                           NAVIGATION.map(({ id, path, label }) => <Box key={id}>{label}</Box>)
+                       }
                    </Grid>
                     <Grid item container justifyContent='end' xs={4}>
                         hello
