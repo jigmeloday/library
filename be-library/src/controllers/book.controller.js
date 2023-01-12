17af7e5fd8a014ea50723ba02a665206b4b77355
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-12 11:27:51 +0600

    book controller get by id

--- be-library/src/controllers/book.controller.js
+++ be-library/src/controllers/book.controller.js
@@ -1,7 +1,23 @@
 const Book = require('../models/book');
+
+
 exports.get_books = (req, res) => {
     Book.find().exec().then((resp) => res.status(200).json({books: resp}).catch((err) => {
             res.status(500).json({message: err})
         })
     )
+}
+
+exports.get_book_by_id = (req, res) => {
+    Book.findById(req.params.id).exec().then((book) =>
+        {
+            if(book){
+                res.status(200).json(book)
+            }else{
+                res.status(404).json({message: 'Not Found'})
+            }
+        }
+    ).catch((err) => {
+        res.status(500).json({ message: err })
+    })
 }
--- be-library/src/routes/books.js
+++ be-library/src/routes/books.js
@@ -18,19 +18,7 @@ const upload = multer({storage});
 
 router.get('/',  bookController.get_books);
 
-router.get('/:id', (req, res) => {
-   Book.findById(req.params.id).exec().then((book) =>
-       {
-           if(book){
-               res.status(200).json(book)
-           }else{
-               res.status(404).json({message: 'Not Found'})
-           }
-       }
-   ).catch((err) => {
-       res.status(500).json({ message: err })
-   })
-});
+router.get('/:id', bookController.get_book_by_id);
 
 router.post('/', routeGuard, upload.single('coverImage') ,(req, res, next) => {
     const book = new Book({
