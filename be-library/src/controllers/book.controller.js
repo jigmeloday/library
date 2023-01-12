dc61cbb9ff5b1ede931fefa43370a0d0832b9b5a
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-12 11:34:06 +0600

    book controller and code refactor

--- be-library/src/controllers/book.controller.js
+++ be-library/src/controllers/book.controller.js
@@ -1,4 +1,5 @@
 const Book = require('../models/book');
+const mongoose = require('mongoose');
 
 
 exports.get_books = (req, res) => {
@@ -39,4 +40,47 @@ exports.update_book = ( req,res, next ) => {
             }
         })
         .catch((error) => res.status(500).json({message:error }))
+}
+
+exports.post_book = (req, res, next) => {
+    const book = new Book({
+        _id:  new mongoose.Types.ObjectId(),
+        title: req.body.title,
+        isbn: req.body.isbn,
+        category: req.body.category,
+        price: req.body.price,
+        quantity: req.body.quantity || 1,
+        summary: req.body.summary,
+        coverImage: req.file.path,
+        author: req.body.author,
+    })
+    book.save()
+        .then((resp) => {
+            if (resp) {
+                res.status(201).json({
+                    message: book
+                });
+            } else{
+                res.status(400).json({
+                    message: 'bad request'
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err })
+        });
+};
+
+exports.delete_book = (req, res, next) => {
+    const id = req.params.id;
+    Book.findByIdAndDelete(id)
+        .exec()
+        .then((resp) => {
+            if (resp) {
+                res.status(201).json({message: resp })
+            }else{
+                res.status(404).json({message: 'Not Found'})
+            }
+        })
+        .catch((err) => res.status(500).json({message: err}))
 }
--- be-library/src/routes/books.js
+++ be-library/src/routes/books.js
@@ -20,48 +20,9 @@ router.get('/',  bookController.get_books);
 
 router.get('/:id', bookController.get_book_by_id);
 
-router.post('/', routeGuard, upload.single('coverImage') ,(req, res, next) => {
-    const book = new Book({
-        _id:  new mongoose.Types.ObjectId(),
-        title: req.body.title,
-        isbn: req.body.isbn,
-        category: req.body.category,
-        price: req.body.price,
-        quantity: req.body.quantity || 1,
-        summary: req.body.summary,
-        coverImage: req.file.path,
-        author: req.body.author,
-    })
-    book.save()
-        .then((resp) => {
-            if (resp) {
-                res.status(201).json({
-                    message: book
-                });
-            } else{
-                res.status(400).json({
-                    message: 'bad request'
-                });
-            }
-        })
-        .catch((err) => {
-            res.status(500).json({ message: err })
-        });
-});
+router.post('/', routeGuard, upload.single('coverImage') , bookController.post_book);
 
-router.delete('/:id',routeGuard, (req, res, next) => {
-    const id = req.params.id;
-    Book.findByIdAndDelete(id)
-        .exec()
-        .then((resp) => {
-            if (resp) {
-                res.status(201).json({message: resp })
-            }else{
-                res.status(404).json({message: 'Not Found'})
-            }
-        })
-        .catch((err) => res.status(500).json({message: err}))
-})
+router.delete('/:id',routeGuard, bookController.delete_book)
 
 router.patch('/:id',routeGuard, bookController.update_book)
 
