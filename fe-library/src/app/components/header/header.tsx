4dee8f5a8d192121ec6f35f8172f6a63a5f4f15f
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-13 17:18:20 +0600

    route set up

--- fe-library/src/app/components/header/header.tsx
+++ fe-library/src/app/components/header/header.tsx
@@ -6,6 +6,8 @@ import { Typography } from '../../shared/components/typography/typography.compon
 import { theme } from '../../../assest/theme';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Button } from '../../shared/components/button/button.component';
+import { ProfileContainer } from '../profile-image/profile-image.style';
+import { ProfileImage } from '../../shared/utils/shared.utils';
 
 export function HeaderComponent() {
     const [active, setActive] = useState<string>('/');
@@ -16,12 +18,12 @@ export function HeaderComponent() {
     }, [url])
     return(
         <HeaderContainer>
-            <Grid item container direction='row'>
+            <Grid item container direction='row' alignItems='center' >
                 <Grid item container xs={5}>
                     Logo
                 </Grid>
-                <Grid item container xs={6} >
-                   <Grid item container direction='row' xs={8} justifyContent='space-around'>
+                <Grid item container xs={7} >
+                   <Grid item container direction='row' xs={9} alignItems='center' justifyContent='space-around'>
 
                              {
                                  NAVIGATION.map(({ id, path, label }) =>
@@ -37,8 +39,18 @@ export function HeaderComponent() {
                              }
 
                    </Grid>
-                    <Grid item container justifyContent='end' xs={4}>
-                       <Button click={() => nav('/authentication/login')} label='Login' variant='contained' color='primary'  />
+                    <Grid item container justifyContent='end' xs={2}>
+                        <ProfileContainer
+                            borderRadius={50}
+                            border={`4px solid
+                             ${theme('light').palette.grey.A100}`}
+                            width='60px' height='60px'
+                            className='cursor--pointer'
+                        >
+                            <img src={ProfileImage} width='100%' height='100%'   className='object-fit--cover border-radius-full'
+                            />
+                        </ProfileContainer>
+                       {/*<Button click={() => nav('/authentication/login')} label='Login' variant='contained' color='primary'  />*/}
                     </Grid>
                 </Grid>
             </Grid>
--- fe-library/src/app/components/profile-image/profile-image.style.ts
+++ fe-library/src/app/components/profile-image/profile-image.style.ts
@@ -1,1 +1,11 @@
-export {}
+import { Grid, styled } from '@mui/material';
+
+export const ProfileContainer = styled(Grid)(
+    ({theme, ...props}) => `
+        width: ${props.width};
+        height: ${props.height};
+        object-fit: cover;
+        border-radius: ${props.borderRadius};
+        border: ${props.border};
+    `
+);
