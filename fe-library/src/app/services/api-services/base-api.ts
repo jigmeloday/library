57aeecb9eaf1ed4ee5cd5b9626268d987f2aa921
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-17 12:59:20 +0600

    base api set up

--- fe-library/src/app/services/api-services/base-api.ts
+++ fe-library/src/app/services/api-services/base-api.ts
@@ -1,3 +1,24 @@
-export const FetchApi = () => {
-    return null
-}
+import { APIResponse, Methods } from '../../shared/models/shared.model';
+
+// const baseUrl = environment.NX_SERVER_URL ;
+
+export const FetchAPI = async <T>( url: string, method: Methods, init?: RequestInit ): Promise<APIResponse<T>> => {
+    return window.fetch( `${''}/${url}`, {
+        method,
+        ...init,
+        credentials: 'include',
+        headers: {
+            'accept-language': 'en',
+        }
+    } ).then( async response => {
+        if ( response.ok || response.status < 400 ) {
+            const json = await response.json();
+            return { data: json };
+        }
+        // convert non-2xx HTTP responses into errors:
+        const json = await response.json();
+        return Promise.resolve( { error: json } );
+    } ).catch( response => {
+        return Promise.resolve( { error: { errors: [response?.toString()] } } );
+    } );
+};
