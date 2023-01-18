8b889ca163513333844465a5ea1b8260ae87ca8e
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-18 16:34:28 +0600

    code refactoring

--- fe-library/src/app/pages/book/book-listing.tsx
+++ fe-library/src/app/pages/book/book-listing.tsx
@@ -4,6 +4,9 @@ import { FetchAPI } from '../../services/api-services/base-api';
 import { BookFacade } from '../../services/facade-service/book-facade';
 import { Typography } from '../../shared/components/typography/typography.component';
 import { BookCard } from './components/book-card.component';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import { Button } from '../../shared/components/button/button.component';
 
 export function BookListing() {
     const [book, setBook] = useState<any>([])
@@ -42,8 +45,16 @@ export function BookListing() {
                                 <CardContent>
                                    <Typography label=' This impressive paella is a perfect party dish and a fun meal to cook
                                         together with your guests. Add 1 cup of frozen peas along with the mussels,
-                                        if you like.' />
+                                        if you like...' />
                                 </CardContent>
+                                <Grid item container direction='row' px='6px' pb='6px'>
+                                    <IconButton aria-label="add to favorites">
+                                        <FavoriteIcon />
+                                    </IconButton>
+                                    <IconButton aria-label="share">
+                                        <ShareIcon />
+                                    </IconButton>
+                                </Grid>
                             </Card>
                         </Grid>
                     )
---
+++ fe-library/src/app/services/model/book.model.ts
@@ -0,0 +1,10 @@
+export interface GetBook {
+    title: string,
+    isbn: number,
+    author: string,
+    price: number,
+    quantity: number,
+    category: string,
+    coverImage: string,
+    summary: string,
+}
