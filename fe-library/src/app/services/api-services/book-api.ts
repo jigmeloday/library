947be49228af108bf0572e1ff8e4bee5e658b245
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-19 12:55:50 +0600

    book details

--- fe-library/src/app/services/api-services/book-api.ts
+++ fe-library/src/app/services/api-services/book-api.ts
@@ -1,4 +1,5 @@
-import { GetBooks } from '../model/book.model';
+import { Book, GetBooks } from '../model/book.model';
 import { FetchAPI } from './base-api';
 
 export const getBooksAPI = () => FetchAPI<GetBooks>('books', 'GET');
+export const getBookAPI = (id: string) => FetchAPI<Book>(`books/${id}`, 'GET');
--- fe-library/src/app/services/facade-service/book-facade.ts
+++ fe-library/src/app/services/facade-service/book-facade.ts
@@ -1,9 +1,12 @@
 import { APIResponse } from '../../shared/models/shared.model';
-import { getBooksAPI } from '../api-services/book-api';
-import { GetBooks } from '../model/book.model';
+import { getBookAPI, getBooksAPI } from '../api-services/book-api';
+import { Book, GetBooks } from '../model/book.model';
 
 export class BookFacade {
     static getBooks(): Promise<APIResponse<GetBooks>> {
         return getBooksAPI();
+    }
+    static getBook(id: string): Promise<APIResponse<Book>> {
+        return  getBookAPI(id);
     }
 }
