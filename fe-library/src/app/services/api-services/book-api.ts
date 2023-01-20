0aca8605f10c81b8de2b5ad2e0733b708cec804a
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-20 14:59:10 +0600

    add book module form binding

--- fe-library/src/app/services/api-services/book-api.ts
+++ fe-library/src/app/services/api-services/book-api.ts
@@ -3,3 +3,4 @@ import { FetchAPI } from './base-api';
 
 export const getBooksAPI = () => FetchAPI<GetBooks>('books', 'GET');
 export const getBookAPI = (id: string) => FetchAPI<Book>(`books/${id}`, 'GET');
+export const createBookAPI = (data: FormData) => FetchAPI<Book>('books', 'POST', { body: data })
