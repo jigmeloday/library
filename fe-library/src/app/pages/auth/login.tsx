0663fe37a53b938085924c4eb9a424408a6f0f31
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-21 17:09:54 +0600

    route set up

--- fe-library/src/app/pages/auth/login.tsx
+++ fe-library/src/app/pages/auth/login.tsx
@@ -1,4 +1,5 @@
 import { Grid } from "@mui/material";
+import { Formik } from "formik";
 import { Button } from "../../shared/components/button/button.component";
 import { Input } from "../../shared/components/input/input.component";
 import { Typography } from "../../shared/components/typography/typography.component";
@@ -10,13 +11,28 @@ export function Login() {
             <Grid item container direction='column'>
                 <Typography label='Login'/>
                 <Grid item container py='20px' direction='column' justifyContent='center' alignItems='cneter'>
-                    <Grid item>
-                      <Input />
-                    </Grid>
-                    <Grid item>
-                        <Input />
-                    </Grid>
-                    <Button label='Login'/>
+                    <Formik
+                        initialValues={{ email: '', password: '' }}
+                        onSubmit={(values) => console.log(values)}
+                    >
+                        {({ handleSubmit, handleChange, values })=>
+                            <>
+                                <Input
+                                    name='email'
+                                    onChange={handleChange}
+                                    value={values.email}
+                                    label='Email' />
+                                <Input
+                                    name='password'
+                                    onChange={handleChange}
+                                    value={values.password}
+                                    type='password'
+                                    label='Password'
+                                />
+                                <Button click={handleSubmit} label='Login'/>
+                            </>
+                        }
+                    </Formik>
                 </Grid>
             </Grid>
         </AuthComponent>
