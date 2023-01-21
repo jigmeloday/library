2d910dc40d0fcabf012471d76a136f59062507db
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-21 17:04:17 +0600

    route set up

--- fe-library/src/app/pages/auth/login.tsx
+++ fe-library/src/app/pages/auth/login.tsx
@@ -1,6 +1,25 @@
+import { Grid } from "@mui/material";
+import { Button } from "../../shared/components/button/button.component";
+import { Input } from "../../shared/components/input/input.component";
+import { Typography } from "../../shared/components/typography/typography.component";
+import { AuthComponent } from "./components/auth.component";
+
 export function Login() {
-    return(
-        <>Login</>
+    return (
+        <AuthComponent>
+            <Grid item container direction='column'>
+                <Typography label='Login'/>
+                <Grid item container py='20px' direction='column' justifyContent='center' alignItems='cneter'>
+                    <Grid item>
+                      <Input />
+                    </Grid>
+                    <Grid item>
+                        <Input />
+                    </Grid>
+                    <Button label='Login'/>
+                </Grid>
+            </Grid>
+        </AuthComponent>
     )
 }
 
