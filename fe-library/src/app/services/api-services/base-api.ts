d653412f7f59a0620c3b9be24b10f467f699b556
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-23 18:59:31 +0600

    authenticated route

--- fe-library/src/app/services/api-services/base-api.ts
+++ fe-library/src/app/services/api-services/base-api.ts
@@ -1,3 +1,4 @@
+import storage from 'redux-persist/es/storage';
 import { APIResponse, Methods } from '../../shared/models/shared.model';
 
 // const baseUrl = environment.NX_SERVER_URL ;
@@ -14,6 +15,9 @@ export const FetchAPI = async <T>( url: string, method: Methods, init?: RequestI
         if ( response.ok || response.status < 400 ) {
             const json = await response.json();
             return { data: json };
+        }
+        if ( response.status === 401 ) {
+            storage.removeItem('persist:root')
         }
         // convert non-2xx HTTP responses into errors:
         const json = await response.json();
