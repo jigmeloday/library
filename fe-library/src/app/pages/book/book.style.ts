0c5d06d6b443664538bb4783b12939e93841232d
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-01-30 17:09:37 +0600

    book detail page

---
+++ fe-library/src/app/pages/book/book.style.ts
@@ -0,0 +1,8 @@
+import { Box, styled } from '@mui/material';
+
+export const BookCard = styled(Box)(
+    ({theme, ...props}) => `
+    height: 470px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+    `
+)
