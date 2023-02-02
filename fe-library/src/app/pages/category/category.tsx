7a5e09c7707adca731c0655b0c888e8b5fe97026
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-02 19:53:01 +0600

    category

--- fe-library/src/app/pages/category/category.tsx
+++ fe-library/src/app/pages/category/category.tsx
@@ -1,6 +1,22 @@
+import { Grid } from '@mui/material';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCategory, selectCategory } from '../../services/states/shared-state/shared.slice';
+
 export function Category() {
+    const categoryList = useSelector(selectCategory);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        categoryList && dispatch(getCategory() as keyof unknown);
+    }, []);
     return(
-        <>hello</>
+       <Grid container item>
+           {
+               categoryList.category?.map(() =>
+                   <>hllo</>
+               )
+           }
+       </Grid>
     )
 }
 
