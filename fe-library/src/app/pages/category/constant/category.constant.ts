7419c9994f40b129bb7287c46d21983a2b4ccb84
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-03 16:15:10 +0600

    category ui

---
+++ fe-library/src/app/pages/category/constant/category.constant.ts
@@ -0,0 +1,28 @@
+export const COLOR = {
+    horror:  '#C72358',
+    romance: '#F584AE',
+    fantasy: '#48B4D5',
+    adventure: '#84CEC1',
+    contemporary: '#1F547D',
+    dystopian: '#95BC8C',
+    mystery: '#9F7986',
+    thriller: '#FFD98A',
+    paranormal: '#D8D8D8',
+    historicalfiction:'#FD9F03',
+    sciencefiction: '#78CBCF',
+    children: '#333333'
+}
+export const BG_COLOR = {
+    horror:  'rgba(199,35,88,0.5)',
+    romance: 'rgba(245,132,174,0.5)',
+    fantasy: 'rgba(72,180,213,0.5)',
+    adventure: 'rgba(132,206,193,0.5)',
+    contemporary: 'rgba(31,84,125,0.5)',
+    dystopian: 'rgba(149,188,140,0.5)',
+    mystery: 'rgba(159,121,134,0.5)',
+    thriller: 'rgba(255,217,138,0.5)',
+    paranormal: 'rgba(216,216,216,0.5)',
+    historicalfiction:'rgba(253,159,3,0.5)',
+    sciencefiction: 'rgba(120,203,207,0.5)',
+    children: 'rgba(51,51,51,0.5)'
+}
