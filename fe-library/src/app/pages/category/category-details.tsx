fc0a45955df18dba61ae1c5093cc6b9ccb43be4b
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-03 17:52:59 +0600

    find book by id

--- fe-library/src/app/pages/category/category-details.tsx
+++ fe-library/src/app/pages/category/category-details.tsx
@@ -1,7 +1,11 @@
+import { Grid } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { SharedFacade } from '../../services/facade-service/shared-facade';
+import { Book } from '../../services/model/book.model';
+import BookListing from '../book/book-listing';
+import { BookCard } from '../book/components/book-card.component';
 
 export function CategoryDetails() {
     const [books, setBooks] = useState<any>();
@@ -9,12 +13,19 @@ export function CategoryDetails() {
     const id = useParams()['id'];
     useEffect(() => {
         SharedFacade.fetchCategoryByID(id as string).then((resp) => {
-            setBooks(resp)
+            setBooks(resp?.data)
         }).catch((error) => console.log(error))
     }, []);
-    console.log(books)
     return(
-        <>hello </>
+        <Grid item container direction='row' py='2px' px='14px' justifyContent='start'>
+            {
+                books?.books?.map((item: Book ) =>
+                    <Grid item py='10px' px='8px' width='24%' key={item._id}>
+                        <BookCard items={item} />
+                    </Grid>
+                )
+            }
+        </Grid>
     )
 }
 
