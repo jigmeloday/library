1fc029c8b11e0486d3501cfdf980ff2f7184adbf
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-06 12:44:47 +0600

    home ui design

--- fe-library/src/app/pages/home/home.tsx
+++ fe-library/src/app/pages/home/home.tsx
@@ -1,38 +1,43 @@
 import { Box, Grid } from '@mui/material';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, lazy } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { getHomeCategory, selectHomeItem } from '../../services/states/shared-state/shared.slice';
 import { Typography } from '../../shared/components/typography/typography.component';
 import { BookCard } from '../book/components/book-card.component';
 
+const NoData = lazy( () => import('../../components/no-data/no-data.component') );
+
 export function Home() {
     const dispatch = useDispatch();
-    const categoryList = useSelector(selectHomeItem);
+    const categoryList = useSelector( selectHomeItem );
     const nav = useNavigate();
-    useEffect(() => {
-        dispatch(getHomeCategory() as keyof unknown)
-    }, []);
+    useEffect( () => {
+        dispatch( getHomeCategory() as keyof unknown )
+    }, [] );
 
-    return(
+    return (
         <Grid container item>
             {
-                categoryList?.books?.map((items: any) =>
-                        <Grid item container key={items?._id} p='20px'>
-                           <Typography label= {items?.name} variant='body1' fontWeight='bold'
-                                       click={() => nav(`/categories/${items._id}`)}
-                                       className='cursor--pointer' />
-                            <Grid item container direction='row'>
-                                {
-                                   items.books.length ?
-                                       items?.books?.map((book: any) =>
-                                           <Box width='20%' padding='12px' >
-                                               <BookCard items={book} key={`${book._id}+${book.title}`}/>
-                                           </Box>): <>NO data</>
-                                }
-                            </Grid>
+                categoryList?.books?.map( ( items: any ) =>
+                    <Grid item container key={ items?._id } p='20px'>
+                        <Typography label={ items?.name } variant='body1' fontWeight='bold'
+                                    click={ () => nav( `/categories/${ items._id }` ) }
+                                    className='cursor--pointer'/>
+                        <Grid item container direction='row'>
+                            {
+                                items.books.length ?
+                                    items?.books?.map( ( book: any ) =>
+                                        <Box width='20%' padding='12px'>
+                                            <BookCard items={ book } key={ `${ book._id }+${ book.title }` }/>
+                                        </Box> ) :
+                                    <Grid item container>
+                                        <NoData/>
+                                    </Grid>
+                            }
                         </Grid>
-                    )
+                    </Grid>
+                )
             }
         </Grid>
     )
