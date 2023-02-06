5e346c4b71f98ee1f511e54f5641a78dca061af9
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-06 17:45:39 +0600

    ui fixes

--- fe-library/src/app/pages/book/book.style.ts
+++ fe-library/src/app/pages/book/book.style.ts
@@ -2,7 +2,7 @@ import { Box, styled } from '@mui/material';
 
 export const BookCard = styled(Box)(
     ({theme, ...props}) => `
-    height: 470px;
+    height: fit-content;
     padding: 4px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
     `
