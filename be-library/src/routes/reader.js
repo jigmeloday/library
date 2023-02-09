08cbf8e213cbf55bd84b19677f1bd6c523dd90fa
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-09 14:49:57 +0600

    autharization

--- be-library/src/routes/reader.js
+++ be-library/src/routes/reader.js
@@ -5,12 +5,12 @@ const routeGuard = require('../route-guard/route-guard');
 const multer = require('multer');
 const upload = multer();
 
-router.post('/', upload.none(), Reader.reader_request);
+router.post('/', routeGuard, upload.none(), Reader.reader_request);
 
 // router.post('/verified', upload.none(), UserController.user_login );
 
 // router.delete('/:id', UserController.user_delete);
 
-router.get('/', Reader.get_reader );
+router.get('/', routeGuard, Reader.get_reader );
 
 module.exports = router;
