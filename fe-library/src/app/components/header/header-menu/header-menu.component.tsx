62ee7ac6fafdc00b5793623e40ca325c6af252aa
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-09 14:28:06 +0600

    nav to profile

--- fe-library/src/app/components/header/header-menu/header-menu.component.tsx
+++ fe-library/src/app/components/header/header-menu/header-menu.component.tsx
@@ -2,18 +2,20 @@ import { Logout, PersonAdd, Settings } from '@mui/icons-material';
 import { Avatar, Divider, ListItemIcon, MenuItem } from '@mui/material';
 import { memo } from 'react'
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import storage from 'redux-persist/es/storage';
 import { clearToken } from '../../../services/states/credential-state/credential.action';
 
 export function HeaderMenu() {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const nav = useNavigate();
     const logOut = () => {
         localStorage.clear();
         dispatch(clearToken())
     }
     return(
         <>
-            <MenuItem >
+            <MenuItem onClick={() =>} >
                 <Avatar /> Profile
             </MenuItem>
             <MenuItem >
