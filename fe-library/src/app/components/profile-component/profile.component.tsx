cb733946a92a9161828a982f8cc7cfe29aea360b
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-14 13:01:25 +0600

    profile

--- fe-library/src/app/components/profile-component/profile.component.tsx
+++ fe-library/src/app/components/profile-component/profile.component.tsx
@@ -1,11 +1,27 @@
 import { Grid } from '@mui/material';
 import { memo } from 'react';
+import { theme } from '../../../assest/theme';
+import { ProfileContainer } from '../profile-image/profile-image.style';
 
 export function ProfileComponent(props: any) {
-    console.log(props)
+
     return(
-       <Grid container item px='38px'>
-jj
+       <Grid container item px='38px' py='32px'>
+           <Grid item container justifyContent='center'>
+               <ProfileContainer
+                   item
+                   xs={2}
+                   borderRadius={50}
+                   border={`4px solid
+                    ${theme('light').palette.grey.A100}`}
+                   width='180px'
+                   height='190px'>
+                   <img src='../../../../images/reading-glasses-animate.svg' width='100%' height='100%' className='object-fit--cover'/>
+               </ProfileContainer>
+               <Grid item container>
+                   
+               </Grid>
+           </Grid>
        </Grid>
     )
 }
