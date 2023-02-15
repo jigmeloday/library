3cf5cbf2576d0c6c8ccecd80ea65e2483ae8e036
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-15 13:03:37 +0600

    get own  profile

--- be-library/src/controllers/profile.controller.js
+++ be-library/src/controllers/profile.controller.js
@@ -1,19 +1,12 @@
 const Profile = require('../models/profile')
 const jwt = require('jsonwebtoken');
 
-exports.get_profile = (req, res, next) => {
-    // const id = req.params.id;
-    // Profile.findOne({uid:id}).exec().then((profile) => {
-    //     if (profile) {
-    //         res.status(200).json({ profile })
-    //     } else{
-    //         res.status(404).json({message: 'Not found'})
-    //     }
-    // }).catch((error) => res.status(500).json({message: error}))
+exports.get_profile = async (req, res, next) => {
     try{
         const token = req.headers.authorization.split(" ")[1];
         const uid = jwt.verify(token, process.env.JWT_KEY).id;
-
+        const profile = await Profile.findOne({ uid: uid });
+        res.status(200).json(profile)
     } catch (error) {
         res.status(500).json({ message: error })
     }
