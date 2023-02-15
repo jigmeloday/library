02dbaa9f30cd3ffea39dd5e9f1aa411d56f09c9d
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-15 13:05:43 +0600

    get own  profile

--- be-library/src/routes/profiles.js
+++ be-library/src/routes/profiles.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 const routeGuard = require('../route-guard/route-guard');
 
-router.get('/', profileController.get_profile );
+router.get('/', routeGuard, profileController.get_profile );
 router.get('/:id', profileController.get_other_profile );
-router.patch('/:id', profileController.update_profile)
+router.patch('/:id', routeGuard, profileController.update_profile)
 
 module.exports = router;
