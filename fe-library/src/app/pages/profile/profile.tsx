1d24c151549d5e2163a7c134f646f9270e2d7b1f
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-15 16:05:30 +0600

    edit profile

--- fe-library/src/app/pages/profile/profile.tsx
+++ fe-library/src/app/pages/profile/profile.tsx
@@ -3,28 +3,38 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ProfileComponent } from '../../components/profile-component/profile.component';
 import { ProfileFacade } from '../../services/facade-service/user-facades';
+import { Button } from '../../shared/components/button/button.component';
 
 export function Profile() {
 
     const [profile, setProfile] = useState<any>();
+    const [edit, setEdit] = useState<boolean>(false);
     useEffect(() => {
         ProfileFacade.fetchProfile().then((resp: any) => {
             setProfile(resp.data);
         })
     }, []);
-
+    const onEdit = () => {
+        setEdit(!edit);
+    }
     return(
        <Grid container item>
+           <Grid item container justifyContent='end' xs={11} py='32px'>
+               <Button label='Edit' variant='contained' click={onEdit} />
+           </Grid>
            <Grid item container direction='column' py='24px' px='36px'>
                <ProfileComponent data={profile} />
-               <Grid item container direction='row' px='36px'>
-                   <Grid>
-                       hello
-                   </Grid><Grid>
-                       hello
-                   </Grid>
-               </Grid>
+               {/*<Grid item container direction='row' px='36px'>*/}
+               {/*    <Grid>*/}
+               {/*        hello*/}
+               {/*    </Grid><Grid>*/}
+               {/*        hello*/}
+               {/*    </Grid>*/}
+               {/*</Grid>*/}
            </Grid>
+           {
+               edit && <>hello</>
+           }
        </Grid>
     )
 }
